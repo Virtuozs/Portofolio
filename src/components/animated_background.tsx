b6134fe851d0456195cff3854f3befdc3cdce815
Virtuozs <contact@virtuozs.com>
2025-06-26 20:33:04 +0700

    style: updating spline file and fixing some 3d animation logic

--- src/components/animated_background.tsx
+++ src/components/animated_background.tsx
@@ -15,41 +15,40 @@ gsap.registerPlugin(ScrollTrigger);
 const STATES = {
   hero: {
     desktop: {
-      scale: { x: 0.2, y: 0.2, z: 0.2 },
-      // position: { x: 500, y: -200, z: 0 },
-      position: { x: -10, y: -100, z: 0 },
+      scale: { x: 0.25, y: 0.25, z: 0.25 },
+      position: { x: 400, y: -200, z: 0 },
       rotation: { x: 0, y: 0, z: 0 },
     },
     mobile: {
       scale: { x: 0.15, y: 0.15, z: 0.15 },
-      position: { x: -900, y: -100, z: 0 },
+      position: { x: 0, y: -200, z: 0 },
       rotation: { x: 0, y: 0, z: 0 },
     },
   },
   skills: {
     desktop: {
-      scale: { x: 0.2, y: 0.2, z: 0.2 },
-      position: { x: -250, y: -20, z: 0 },
+      scale: { x: 0.4, y: 0.4, z: 0.4 },
+      position: { x: 0, y: -40, z: 0 },
       rotation: {
         x: 0,
-        y: Math.PI / 3,
+        y: Math.PI / 12,
         z: 0,
       },
     },
     mobile: {
-      scale: { x: 0.15, y: 0.15, z: 0.15 },
-      position: { x: -900, y: -100, z: 0 },
+      scale: { x: 0.2, y: 0.2, z: 0.2 },
+      position: { x: 0, y: -40, z: 0 },
       rotation: {
         x: 0,
-        y: Math.PI / 3,
+        y: Math.PI / 6,
         z: 0,
       },
     },
   },
   projects: {
     desktop: {
-      scale: { x: 0.25, y: 0.25, z: 0.25 },
-      position: { x: -400, y: -100, z: 0 },
+      scale: { x: 0.3, y: 0.3, z: 0.3 },
+      position: { x: 0, y: -40, z: 0 },
       rotation: {
         x: Math.PI,
         y: Math.PI / 3,
@@ -57,8 +56,8 @@ const STATES = {
       },
     },
     mobile: {
-      scale: { x: 0.16, y: 0.16, z: 0.16 },
-      position: { x: -900, y: -100, z: 0 },
+      scale: { x: 0.18, y: 0.18, z: 0.18 },
+      position: { x: 0, y: 150, z: 0 },
       rotation: {
         x: Math.PI,
         y: Math.PI / 3,
@@ -455,7 +454,7 @@ const AnimatedBackground = () => {
         <Suspense fallback={<></>}>
           <Spline
             ref={splineContainer}
-            scene="/keyboard_final.splinecode"
+            scene="/keyboard_scene.splinecode"
             onLoad={(app: Application) => {
               setSplineApp(app);
               setTimeout(() => bypassLoading(), 300);
